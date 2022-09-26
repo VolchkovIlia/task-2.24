caec9d3f11aadb128d20c9587cabc144d6b8738e
VolchkovIlia <113889753+VolchkovIlia@users.noreply.github.com>
2022-09-26 12:04:09 +0300

    Add files via upload

---
+++ 2.24/2.24/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+public class Mainclass
+{
+public static void Main()
+    {
+        double x;
+        x = int.Parse("2" + ((372 - 2) / 10));
+        Console.WriteLine("{0}", x);
+    }
+}
+
